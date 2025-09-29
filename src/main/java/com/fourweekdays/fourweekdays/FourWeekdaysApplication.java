d482467c0dac670f6b802fc01699f4e9d67f434b
tipsyboy <gudah13@naver.com>
2025-09-29 15:10:41 +0900

    init project

---
+++ src/main/java/com/fourweekdays/fourweekdays/FourWeekdaysApplication.java
@@ -0,0 +1,13 @@
+package com.fourweekdays.fourweekdays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourWeekdaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourWeekdaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourweekdays/fourweekdays/FourweekdaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourweekdays.fourweekdays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourweekdaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
