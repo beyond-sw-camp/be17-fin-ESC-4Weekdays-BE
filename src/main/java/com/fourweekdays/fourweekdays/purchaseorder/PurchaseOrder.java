cd5c187b7f7b96844ce7a3e2ea8cc18fc2301d50
tipsyboy <gudah13@naver.com>
2025-10-02 17:23:47 +0900

    feat: 발주(PurchaseOrder) 엔티티를 구현한다. ---
    feat: 발주(PurchaseOrder) 엔티티를 구현한다.
    feat: 발주(PurchaseOrder) 상태 enum 클래스를 구현한다.

---
+++ src/main/java/com/fourweekdays/fourweekdays/purchaseorder/PurchaseOrder.java
@@ -0,0 +1,28 @@
+package com.fourweekdays.fourweekdays.purchaseorder;
+
+import com.fourweekdays.fourweekdays.franchise.FranchiseStore;
+import com.fourweekdays.fourweekdays.product.Product;
+import jakarta.persistence.*;
+
+@Entity
+public class PurchaseOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private FranchiseStore store; // 가맹점 / 발주 요청자
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity; // 발주 수량
+    private Long amount; // 발주 금액
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+}
---
+++ src/main/java/com/fourweekdays/fourweekdays/purchaseorder/Status.java
@@ -0,0 +1,8 @@
+package com.fourweekdays.fourweekdays.purchaseorder;
+
+public enum Status {
+    REQUESTED, // 발주 요청 
+    APPROVED, // 승인
+    COMPLETED, // 완료
+    CANCELLED // 취소
+}
