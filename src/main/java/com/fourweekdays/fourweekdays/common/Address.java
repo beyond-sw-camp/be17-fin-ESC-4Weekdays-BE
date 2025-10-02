65682380bf1c4514788eed9930a4a9e09c37eb25
tipsyboy <gudah13@naver.com>
2025-10-02 17:08:49 +0900

    feat: Address VO를 구현한다. ---
    feat: Address VO를 구현한다.

---
+++ src/main/java/com/fourweekdays/fourweekdays/common/Address.java
@@ -0,0 +1,14 @@
+package com.fourweekdays.fourweekdays.common;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String zipcode; // 우편번호
+    private String street; // 도로명 주소
+    private String detail; // 상세 주소 (건물명, 호수 등)
+    private String city; // 도시
+    private String country; // 국가 코드 (예: KR, US)
+
+}
