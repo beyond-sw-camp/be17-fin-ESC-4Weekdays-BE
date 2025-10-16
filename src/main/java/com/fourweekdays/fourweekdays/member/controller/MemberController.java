6f13c04bd93735d4c29864e8865a4bbfc462e3fa
wonzzu <wonzzu@naver.com>
2025-10-16 15:42:58 +0900

    feat: member 컨트롤러 dto swagger 작업

--- src/main/java/com/fourweekdays/fourweekdays/member/controller/MemberController.java
+++ src/main/java/com/fourweekdays/fourweekdays/member/controller/MemberController.java
@@ -6,19 +6,27 @@ import com.fourweekdays.fourweekdays.member.model.dto.MemberSignUpDto;
 import com.fourweekdays.fourweekdays.member.model.dto.MemberUpdateDto;
 import com.fourweekdays.fourweekdays.member.service.MemberService;
 import com.fourweekdays.fourweekdays.product.dto.request.ProductUpdateDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "직원 관리")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/member")
 public class MemberController {
+
     private final MemberService memberService;
 
     // 회원 등록
+    @Operation(
+            summary = "신규 직원 등록",
+            description = "관리자 권한으로 신규 직원의 정보를 시스템에 등록한다."
+    )
     @PostMapping("/signup")
     public ResponseEntity<BaseResponse<String>> register(@RequestBody MemberSignUpDto dto) {
         memberService.register(dto);
@@ -26,6 +34,10 @@ public class MemberController {
     }
 
     // 직원 목록 조회
+    @Operation(
+            summary = "직원 목록 조회",
+            description = "시스템에 등록된 직원들의 목록을 불러온다."
+    )
     @GetMapping("/list")
     public ResponseEntity<BaseResponse<List<MemberResponseDto>>> getMemberList() {
         List<MemberResponseDto> productList = memberService.getMemberList();
@@ -33,12 +45,20 @@ public class MemberController {
     }
 
     //직원 상세 조회
+    @Operation(
+            summary = "직원의 상세 정보를 조회",
+            description = "직원의 고유 ID를 사용하여 해당 직원의 상세 정보를 조회한다."
+    )
     @GetMapping("/{id}")
     public ResponseEntity<BaseResponse<MemberResponseDto>> getByMemberId(@PathVariable Long id) {
         return ResponseEntity.ok(BaseResponse.success(memberService.getMemberDetails(id)));
     }
 
     //직원 정보 수정
+    @Operation(
+            summary = "직원의 정보를 수정",
+            description = "관리자 권한으로 기존 직원의 정보를 수정해 적용한다."
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<BaseResponse<Long>> updateMember(@PathVariable Long id,
                                                             @RequestBody MemberUpdateDto requestDto) {
--- src/main/java/com/fourweekdays/fourweekdays/member/model/dto/MemberLoginDto.java
+++ src/main/java/com/fourweekdays/fourweekdays/member/model/dto/MemberLoginDto.java
@@ -1,5 +1,6 @@
 package com.fourweekdays.fourweekdays.member.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,9 +13,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberLoginDto {
 
+    @Schema(description = "로그인 이메일", example = "test@test.com")
     @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
 
+    @Schema(description = "로그인 비밀번호", example = "qwer1234")
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
