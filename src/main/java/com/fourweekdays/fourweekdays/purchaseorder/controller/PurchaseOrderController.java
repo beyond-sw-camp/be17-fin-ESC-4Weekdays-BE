276cee8fdcc6fff4e35d150103bea24dde846d61
wonzzu <wonzzu@naver.com>
2025-10-16 16:20:13 +0900

    feat:컨트롤러 및 DTO swagger 추가

--- src/main/java/com/fourweekdays/fourweekdays/purchaseorder/controller/PurchaseOrderController.java
+++ src/main/java/com/fourweekdays/fourweekdays/purchaseorder/controller/PurchaseOrderController.java
@@ -4,12 +4,15 @@ import com.fourweekdays.fourweekdays.common.BaseResponse;
 import com.fourweekdays.fourweekdays.purchaseorder.model.dto.request.PurchaseOrderCreateDto;
 import com.fourweekdays.fourweekdays.purchaseorder.model.dto.response.PurchaseOrderReadDto;
 import com.fourweekdays.fourweekdays.purchaseorder.service.PurchaseOrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "발주 기능")
 @RequestMapping("/api/purchase-orders")
 @RequiredArgsConstructor
 @RestController
@@ -17,16 +20,28 @@ public class PurchaseOrderController {
 
     private final PurchaseOrderService purchaseOrderService;
 
+    @Operation(
+            summary = "신규 발주 등록",
+            description = "신규 발주를 등록한다."
+    )
     @PostMapping
     public ResponseEntity<BaseResponse<Long>> purchaseRequest(@RequestBody PurchaseOrderCreateDto requestDto) {
         return ResponseEntity.ok(BaseResponse.success(purchaseOrderService.create(requestDto)));
     }
 
+    @Operation(
+            summary = "발주 목록 조회",
+            description = "전체 발주 내역을 조회한다."
+    )
     @GetMapping
     public ResponseEntity<BaseResponse<List<PurchaseOrderReadDto>>> purchaseOrderList() {
         return ResponseEntity.ok(BaseResponse.success(purchaseOrderService.findAll()));
     }
 
+    @Operation(
+            summary = "발주 상세 조회",
+            description = "발주 ID를 기준으로 해당 발주 상세 정보를 조회한다."
+    )
     @GetMapping("/{id}")
     public ResponseEntity<BaseResponse<PurchaseOrderReadDto>> purchaseOrderDetail(@PathVariable Long id) {
         return ResponseEntity.ok(BaseResponse.success(purchaseOrderService.findByPurchaseOrderId(id)));
